022284ce3a757c4e152ec3dc1c7fca0025494059
ghassen-fatnassi <oneinbillion77@gmail.com>
2024-10-24 11:40:36 +0100

    don't know what i'm doing

--- JobScheduleRL/Greedy.cpp
+++ JobScheduleRL/Greedy.cpp
@@ -3,42 +3,46 @@
 #include <queue>
 #include <algorithm>
 
-struct Job {
+struct Job
+{
     int id;
     int processingTime;
-    
+
     Job(int id, int processingTime) : id(id), processingTime(processingTime) {}
 };
 
-void printSchedule(const std::vector<Job>& jobs, const std::string& strategy) {
+void printSchedule(const std::vector<Job> &jobs, const std::string &strategy)
+{
     std::cout << strategy << " Schedule:\n";
     int currentTime = 0;
-    for (const auto& job : jobs) {
+    for (const auto &job : jobs)
+    {
         currentTime += job.processingTime;
         std::cout << "Job " << job.id << " (Processing Time: " << job.processingTime << ") -> Completed at: " << currentTime << "\n";
     }
     std::cout << "\n";
 }
 
-std::vector<Job> greedySchedule(std::vector<Job> jobs) {
-    std::sort(jobs.begin(), jobs.end(), [](const Job& a, const Job& b) {
-        return a.processingTime < b.processingTime;
-    });
+std::vector<Job> greedySchedule(std::vector<Job> jobs)
+{
+    std::sort(jobs.begin(), jobs.end(), [](const Job &a, const Job &b)
+              { return a.processingTime < b.processingTime; });
     return jobs;
 }
 
-std::vector<Job> fifoSchedule(const std::vector<Job>& jobs) {
+std::vector<Job> fifoSchedule(const std::vector<Job> &jobs)
+{
     return jobs;
 }
 
-int main() {
+int main()
+{
     std::vector<Job> jobs = {
         Job(1, 10),
         Job(2, 5),
         Job(3, 8),
         Job(4, 2),
-        Job(5, 7)
-    };
+        Job(5, 7)};
 
     std::vector<Job> fifoJobs = fifoSchedule(jobs);
     printSchedule(fifoJobs, "FIFO");
